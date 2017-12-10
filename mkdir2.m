5a9377bd2a0f03fa708003f07b11642a4088ffec
Dr. Kyaw Kyaw Htike @ Ali Abdul Ghafur <34255453+Kyaw-Kyaw-Htike@users.noreply.github.com>
2017-12-10 22:10:34 +0800

    Update mkdir2.m

--- mkdir2.m
+++ mkdir2.m
@@ -1,5 +1,5 @@
 function mkdir2(str_dir)
-% Copyright (C) Dr. Kyaw Kyaw Htike @ Ali Abdul Ghafur. All rights reserved.
+% Copyright (C) 2017 Kyaw Kyaw Htike @ Ali Abdul Ghafur. All rights reserved.
 % similar to the built-in mkdir with the difference that 
 % if overwrites any existing directory with the given name.
 % IOW, mkdir2 always make a new directory (with no contents therein)
